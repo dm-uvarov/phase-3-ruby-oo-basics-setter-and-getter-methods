f7ea571900c96835c5a417c520edc82517ea15f1
Jimmy <dm.a.uvarov@gmail.com>
2022-02-24 17:04:42 -0500

    commit tree b6ac8a11b892ce1f0db6f3760e46db85b58a12cf

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
+    
+    def name=(value)
+        @name = value
+    end
 
+    def name
+        @name
+    end
+
+    def job=(occupy)
+        @job =occupy
+    end
+
+    def job
+        @job
+    end
 end
